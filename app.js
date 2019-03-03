f0f7afb630289ee3aa22c21babee71dda4a4bf9a
almog1009 <almoggal10@gmail.com>
2019-03-03 23:09:00 +0200

    added consts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var news = require("./controller/news/news");
+const NEWS_CONSTS = require('../../data-const/news-consts');
 var app = express();
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
@@ -10,7 +12,7 @@ app.get("/", function (req, res){
 })
 
 app.get("/newsJson", function(req, res){
-    news.TopHeadlines('politics' , 'en', 'us', next => {
+    news.TopHeadlines(NEWS_CONSTS.category, NEWS_CONSTS.language, NEWS_CONSTS.country, next => {
         res.send(news.ReturnedContent(next));
         }
     );
