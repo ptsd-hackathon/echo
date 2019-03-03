245d2204d47f4298b8d04abdb8656fcf9df55e96
almog1009 <almoggal10@gmail.com>
2019-03-03 21:53:17 +0200

    echoo

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/", function (req, res){
 })
 
 app.get("/newsJson", function(req, res){
-    news.TopHeadlines(category , language, country);
+    res.send(news.TopHeadlines('politics' , 'en', 'us'));
 
 })
--- controller/news/news.js
+++ controller/news/news.js
@@ -14,28 +14,25 @@ const newsapi = new NewsAPI(KEY_API);
 // To query top headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
 function TopHeadlines(category, language, country){
-    newsapi.v2.topHeadlines({
+    return newsapi.v2.topHeadlines({
         // q: 'trump',
         category: category,
         language: language,
         country: country,
     }).then(response => {
-        console.log(response.articles[0].title + response.articles[0].description + '\n');
+        return ((response.articles[0].title + response.articles[0].description + '\n'));
     });
 }
-
 function EveryArticle(fromDate, toDate, language, sources, sortBy){
-    newsapi.v2.everything({
+    return newsapi.v2.everything({
         sources: sources,
         from: fromDate,
         to: toDate,
         language: language,
         sortBy: sortBy,
     }).then(response => {
-        console.log(response.articles[0].title + " " + response.articles[0].description + '\n');
+        return (response.articles[0].title + " " + response.articles[0].description + '\n');
     });
 }
 
 module.exports = { EveryArticle, TopHeadlines};
-
-// TopHeadlines(category , language, country);
