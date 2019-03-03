dc9712face9b6523b61cd2fb5128bed7de1838b4
maoz <maoz.s65@gmail.com>
2019-03-03 20:44:25 +0200

    d

--- controller/Action/findAction.ts
+++ controller/Action/findAction.ts
@@ -1,6 +1,14 @@
 var words = require('./safeWords.json');
+var tcom = require('thesaurus-com');
 
-export function isProblemAction(action:string): boolean{
+export function isProblemAction(action:string){
     let safe:string[] = words.words;
+    words.words.forEach(word => {
+        tcom.search(word).synonyms.forEach(x => {
+            safe.push(x);
+        });;
+    });;
+
     return safe.includes(action);
 }
+
