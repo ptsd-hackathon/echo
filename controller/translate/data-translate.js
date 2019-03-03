a8c88cac2ecaa3fc6be37d531b91ac8349b30c3c
almog1009 <almoggal10@gmail.com>
2019-03-03 16:06:13 +0200

    added post

--- controller/translate/data-translate.js
+++
@@ -1,9 +0,0 @@
-// var fs = require("fs");
-// console.log("\n *START* \n");
-// // var content = fs.readFileSync("content.txt");
-// var content = "{ 'key' : 'almog'}";
-// var jsonParse = JSON.parse(content);
-// console.log("key : " + jsonParse.key);
-
-import {KEY_API} from '../../data-const/post-consts';
-
