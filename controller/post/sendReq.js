cf18ee8a386d66d9cc2e746c9e0799f44a5eb187
almog1009 <almoggal10@gmail.com>
2019-03-03 17:15:03 +0200

    added some folders.

--- controller/post/sendReq.js
+++ controller/post/sendReq.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 function PostRequest(postUrl, formJson) {
     request.post({ url: postUrl, form: formJson }, function (err, res, body) {
-        console.log(body);
+        console.log(JSON.parse(body).text);
         // console.log(res);
         console.log(res.statusCode);
     });
