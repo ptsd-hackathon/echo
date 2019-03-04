d323679295395cb1233e862fae406e719b19bdb7
ronenpi18 <ronenpi18@github.com>
2019-03-04 03:47:44 +0200

    bla

--- controller/nlp/nlp.js
+++ controller/nlp/nlp.js
@@ -1,13 +1,53 @@
-const KEY_API = require('../../data-const/post-consts').KEY_API_TRANSLATE;
-const HOST = require('../../data-const/post-consts').HOST_TRANSLATE;
-var sendReq = require('../post/sendReq');
+const TextRazor = require('textrazor');
+const textRazor = new TextRazor('d57ee749a1c8ef620d105548b7611647534b4519a46cdd78ed2bc2ec')
+const content = 'The Federal Reserve is the enemy of Ron Paul.'
+const options = { extractors: 'entities,topics,relations' }
 
-var postUrl = "https://" + HOST;
-var newsDoc = 'מה קורה גבר';
-var formJson = {key : KEY_API, lang : 'he-en', text: newsDoc};
-var headers = {}
+function getRootOfSentence(content,options) {
+    return textRazor.exec(content, options)
+        .then(value => {
+            let mashu = [];
 
-/**
- * sending post request
- */
-sendReq.PostRequest(postUrl, formJson, headers);
+            for(let i = 0; i < value.response.sentences.length; i++){
+                for(let j=0; j<value.response.sentences[i].words.length;j++){
+                    let relationToParent = value.response.sentences[i].words[j]
+                    if(!relationToParent.hasOwnProperty("relationToParent")){
+                        mashu.push(value.response.sentences[i].words[j].token);
+                    }
+                }
+            }
+
+            return mashu;
+
+        })
+        .catch(err => console.error(err))
+
+}
+
+function getLocationFromMetadata(content,options) {
+    return textRazor.exec(content, options)
+        .then(value => {
+            let mashu = [];
+
+            for(let i = 0; i < value.response.entities.length; i++){
+                // for(let j=0; j<value.response.entities[i].type.length;j++){
+                //     let relationToParent = value.response.sentences[i].words[j]
+                    // if(value.response.entities[i].type.indexOf("Place") > -1){
+                    //     mashu.push(value.response.sentences[i].words[j].token);
+                    // }
+                    if(value.response.entities[i].type.indexOf("City") > -1){
+                        mashu.push(value.response.entities[i].entityId);
+                    }
+                // }
+            }
+
+            return mashu;
+
+        })
+        .catch(err => console.error(err))
+}
+
+
+getLocationFromMetadata("hello, my name is Tel-Aviv",options).then(answer => {
+    console.log(answer);
+});
